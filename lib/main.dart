51c5857d76e33c9a355673eae11aed1b22dcb32f
SCHAC <kouakouchristevann@gmail.com>
2026-07-26 11:01:44 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/theme/app_theme.dart';
 import 'features/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MobilitiApp());
