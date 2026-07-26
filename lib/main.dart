1ff1cb9024fa5350b93b3d51bd8647ead746c32d
SCHAC <kouakouchristevann@gmail.com>
2026-07-26 11:02:44 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'features/splash/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  runApp(const MobilitiApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
 Firebase.initializeApp(
